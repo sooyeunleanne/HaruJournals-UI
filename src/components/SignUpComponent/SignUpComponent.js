d00d50f9eee4a03d4d68313fbf82565a80550107
Leanne <sooyeunleanne@gmail.com>
2024-09-26 20:30:00 -0400

    connecting sign up to mongoDB

--- src/components/SignUpComponent/SignUpComponent.js
+++ src/components/SignUpComponent/SignUpComponent.js
@@ -1,15 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function SignUpComponent( {onCloseClick, onLogInClick} ) {
-    return (
-        <div className='popup-container'>
-            <button className='close-button' onClick={onCloseClick}> X </button>
-            <h1>Sign-up</h1>
-            <p>Username</p>
-            <textarea className='login-textarea'></textarea>
-            <p>Password</p>
-            <textarea className='login-textarea'></textarea>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
-            <button className='connect-to-button' onClick={onLogInClick}>Log-in</button>            
-        </div>
+    const handleSignup = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/users', { username, password });
+            setMessage(response.data.message);
+        } catch (error) {
+            setMessage(error.response?.data.message || 'An error occurred');
+        }
+    };
+
+    return (
+        <form>
+            <div className='popup-container'>
+                <button className='close-button' onClick={onCloseClick}> X </button>
+                <h1>Sign-up</h1>
+                <p>Username</p>
+                <input 
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className='login-textarea'
+                required></input>
+                <p>Password</p>
+                <input 
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='login-textarea'
+                required></input>
+
+                <button className='connect-to-button' onClick={handleSignup}>Sign Up</button>            
+            </div>
+        </form>
+        
     )
 }
